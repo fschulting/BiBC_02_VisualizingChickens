61b85591b5c20467dda4afae0dd4c3c9f3c0cc0e
Famke Schulting <fschulting@users.noreply.github.com>
2022-09-22 10:56:17 +0200

    Comitted our right figure (weight against dates), since the one before was the wrong one..

--- BiBC_02_R_Chickens.R
+++ BiBC_02_R_Chickens.R
@@ -1,12 +1,11 @@
 library(ggplot2)
 
 setwd("~/Desktop")
-raw_data <- read.csv("Scorings.csv",sep = ";")
-chicken_data <- na.omit(raw_data)
-chicken_data
+chicken_data <- read.csv("Scorings.csv",sep = ";")
 
+chicken_data$Color[chicken_data$Color == ''] <- 'Unknown'
 
-ggplot(data = chicken_data) + 
-  geom_point(mapping = aes(x = Head, y = Feet))
+chicken_data$Date <- factor(chicken_data$Date, levels=c('30/04/2020','08/08/2020','29/08/2020', '06/09/2020'))
 
-
+ggplot(data = chicken_data, aes(x = Date, y = Weight, color = Sex, shape = Color)) + 
+  geom_jitter(width=0.25, size=3)
